925275300eead05202b120bc81a1a9877d572ad6
sahasam <munamalasahas@gmail.com>
2020-08-11 22:02:31 -0700

    added recursive breadth first search algorithm

--- wikigamesolver/crawler/crawler.py
+++ wikigamesolver/crawler/crawler.py
@@ -4,7 +4,7 @@ Usage: wikigamesolver crawl [options]
 Options:
     -h, --help                          print this message
     -s, --start-page <url>              start page for wikipedia crawler [default: https://en.wikipedia.org/wiki/Python_(programming_language)]
-    -e, --end-page <url>                end page for wikipedia crawler [default: https://en.wikipedia.org/wiki/Web_scraping]
+    -e, --end-page <url>                end page for wikipedia crawler [default: https://en.wikipedia.org/wiki/London_Stock_Exchange]
 
 """
 import logging
@@ -16,13 +16,19 @@ from docopt import docopt
 logger = logging.getLogger(__name__)
 
 
-def bfs (start_page, end_page) :
-    link_list = map(lambda x: x.lower().replace(' ', '_'), wikipedia.page(start_page).links)
-    if end_page in list(link_list):
+def bfs (start_page, end_page, depth=0, trace=[]) :
+    link_list = list(map(lambda x: x.lower().replace(' ', '_'), wikipedia.page(start_page).links))
+    if end_page in link_list:
         logger.debug(f"FOUND: {end_page} is in {start_page}")
+        return True
     else:
         logger.debug(f"NOT FOUND: {end_page} not found in {start_page}. Moving down the tree")
-
+        if depth >= 5:
+            return False
+        for link in link_list :
+            if bfs(link, end_page, depth + 1, trace) :
+                return True
+        return False
 
 def main():
     logger.debug("starting crawler")
